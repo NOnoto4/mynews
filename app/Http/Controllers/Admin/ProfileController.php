c26f482e97a5ade6528f16bca50c17735de1930f
ONO <onoto04@gmail.com>
2023-01-15 02:32:29 +0000

    14まで

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,26 +5,42 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class ProfileController extends Controller
+// 以下の1行を追記することで、News Modelが扱えるようになる
+use App\Models\Profile;
+
+class NewsController extends Controller
 {
-    //
     public function add()
     {
         return view('admin.profile.create');
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        // 以下を追記
+        // Validationを行う
+        $this->validate($request, Profile::$rules);
+
+        $profile = new Profile;
+        $form = $request->all();
+
+        // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $profile->image_path = basename($path);
+        } else {
+            $profile->image_path = null;
+        }
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+
         return redirect('admin/profile/create');
-    }
-
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
-
-    public function update()
-    {
-        return redirect('admin/profile/edit');
     }
 }
